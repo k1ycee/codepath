baaead71d87d3bb4477549c7c730967186a30ec8
k1ycee <chiagozieani145@gmail.com>
2022-07-30 00:26:35 +0100

    first commit

---
+++ app/src/main/java/com/example/codepath/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.codepath
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Toast.makeText(this, "Hi, we just met", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
